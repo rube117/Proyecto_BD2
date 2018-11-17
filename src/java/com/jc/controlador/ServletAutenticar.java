8a2a4a3b62b71c5f605cd025a32860c47d64b5f8
campitos <rapidclimate@gmail.com>
2018-11-17 09:11:54 -0600

    asasa

---
+++ src/java/com/jc/controlador/ServletAutenticar.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jc.controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author campitos
+ */
+public class ServletAutenticar extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out=response.getWriter();
+        out.println("Mi primer servlet");
+        out.println("La ip de acceso es "+request.getRemoteAddr());
+        
+        
+    }
+}
